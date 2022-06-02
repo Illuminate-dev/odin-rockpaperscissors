4cf18f8f38cf3be6fce6484beccbc3bb66b3fdb6
Henry Beveridge <henrydbeveridge@gmail.com>
2022-06-01 18:23:09 -0700

    Add capitalization

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 
+function capitalize(string) {
+    return string[0].toUpperCase().concat(string.toLowerCase().substr(1));
+}
+
+
 function computerPlay() {
     randInt = Math.floor(Math.random() * 3);
     switch(randInt) {
@@ -15,15 +20,18 @@ function playRound(playerSelection, computerSelection) {
     const playerSelectionLower = playerSelection.toLowerCase();
     const computerSelectionLower = computerSelection.toLowerCase();
 
+    const playerSelectionCapitalized = capitalize(playerSelection);
+    const computerSelectionCapitalized = capitalize(computerSelection);
+
     if (playerSelectionLower === computerSelectionLower) {
-        return `Tie Game! ${playerSelectionLower} ties with ${computerSelectionLower}.`;
+        return `Tie Game! ${playerSelectionCapitalized} ties with ${computerSelectionCapitalized}.`;
     }
     if (
         (playerSelectionLower === 'rock' && computerSelectionLower === 'scissors') ||
         (playerSelectionLower === 'scissors' && computerSelectionLower === 'paper') ||
         (playerSelectionLower === 'paper' && computerSelectionLower === 'rock')
     ) {
-        return `You Win! ${playerSelectionLower} beats ${computerSelectionLower}.`
+        return `You Win! ${playerSelectionCapitalized} beats ${computerSelectionCapitalized}.`
     }
-    return `You Lose! ${computerSelectionLower} beats ${playerSelectionLower}.`
+    return `You Lose! ${computerSelectionCapitalized} beats ${playerSelectionCapitalized}.`
 }
