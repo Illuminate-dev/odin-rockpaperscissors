1e6bf22081565c5a32044bc80626e557e52d1b13
Henry Beveridge <henrydbeveridge@gmail.com>
2022-06-01 18:56:40 -0700

    Add game

--- index.js
+++ index.js
@@ -23,15 +23,58 @@ function playRound(playerSelection, computerSelection) {
     const playerSelectionCapitalized = capitalize(playerSelection);
     const computerSelectionCapitalized = capitalize(computerSelection);
 
+    if (
+        (playerSelectionLower !== 'rock') &&
+        (playerSelectionLower !== 'scissors') &&
+        (playerSelectionLower !== 'paper')
+    ) {
+        let playerInput = prompt("Thats not Rock, Paper, or Scissors!\nChoose one.")
+        return playRound(playerInput, computerPlay());
+    }
+
     if (playerSelectionLower === computerSelectionLower) {
-        return `Tie Game! ${playerSelectionCapitalized} ties with ${computerSelectionCapitalized}.`;
+        return [`Tie Game! ${playerSelectionCapitalized} ties with ${computerSelectionCapitalized}.`, 'tie'];
     }
     if (
         (playerSelectionLower === 'rock' && computerSelectionLower === 'scissors') ||
         (playerSelectionLower === 'scissors' && computerSelectionLower === 'paper') ||
         (playerSelectionLower === 'paper' && computerSelectionLower === 'rock')
     ) {
-        return `You Win! ${playerSelectionCapitalized} beats ${computerSelectionCapitalized}.`
+        return [`You Win! ${playerSelectionCapitalized} beats ${computerSelectionCapitalized}.`, 'player']
     }
-    return `You Lose! ${computerSelectionCapitalized} beats ${playerSelectionCapitalized}.`
+    return [`You Lose! ${computerSelectionCapitalized} beats ${playerSelectionCapitalized}.`, 'computer']
 }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerInput = prompt("Rock, Paper, or Scissors?");
+        const round = playRound(playerInput, computerPlay());
+
+        console.log(round[0]);
+
+        if (round[1] === 'player') {
+            playerScore++;
+        } else if (round[1] === 'computer') {
+            computerScore++;
+        } else {
+            i--;
+        }
+
+        console.log(
+            `It's ${playerScore}-${computerScore}!`
+            .concat(playerScore > computerScore ? " You're up!" : (
+            playerScore !== computerScore ? 
+            " The computer's winning!" : " Tie game!"
+            )));
+        5-i !== 1 ? console.log(`${5-i} rounds left!`) : null;
+
+    }
+
+    playerScore > computerScore ? console.log("You Won!") : console.log("You Lost.");
+
+}
+
+game();
